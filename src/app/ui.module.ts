ba5500632434abe528fc175b6f91b2ecb159041a
Кузнецов М.П <kuznecovmp@ugpa.ru>
2021-08-18 21:26:26 +0300

    taiga added

---
+++ src/app/ui.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { TuiAccordionModule } from '@taiga-ui/kit';
+
+@NgModule({
+    imports: [
+        TuiAccordionModule,
+    ],
+    exports: [
+        TuiAccordionModule,
+    ],
+    declarations: [],
+    providers: [],
+})
+export class UIModule { }
