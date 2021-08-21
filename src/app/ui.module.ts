ac44a97a279dd1af36d834de6ad09effe490836c
Кузнецов М.П <kuznecovmp@ugpa.ru>
2021-08-21 10:38:50 +0300

    overwritten colors && tui test

--- src/app/ui.module.ts
+++ src/app/ui.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
+
 import { TuiAccordionModule } from '@taiga-ui/kit';
+import { TuiButtonModule } from '@taiga-ui/core';
+import { TuiCalendarModule } from '@taiga-ui/core';
 
 @NgModule({
     imports: [
         TuiAccordionModule,
+        TuiButtonModule,
+        TuiCalendarModule,
     ],
     exports: [
         TuiAccordionModule,
+        TuiButtonModule,
+        TuiCalendarModule,
     ],
     declarations: [],
     providers: [],
