55a933decb29a32a612cb0cded6d175193c80632
Кузнецов М.П <kuznecovmp@ugpa.ru>
2021-09-06 15:06:38 +0300

    set russian locale

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,8 @@ import { NgModule, InjectionToken } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { environment } from '../../environments/environment';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TUI_LANGUAGE, TUI_RUSSIAN_LANGUAGE } from '@taiga-ui/i18n';
+import { of } from 'rxjs';
 
 export const API_URL = new InjectionToken<string>('API_URL');
 
@@ -18,7 +20,14 @@ export const API_URL = new InjectionToken<string>('API_URL');
         FormsModule,
     ],
     providers: [
-        { provide: API_URL, useValue: environment.API_URL },
+        {
+            provide: API_URL,
+            useValue: environment.API_URL
+        },
+        {
+            provide: TUI_LANGUAGE,
+            useValue: of(TUI_RUSSIAN_LANGUAGE),
+        },
     ],
 })
 export class CoreModule { }
