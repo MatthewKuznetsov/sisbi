858e07fd0ae92547c7f553059de399f7024a04f3
Кузнецов М.П <kuznecovmp@ugpa.ru>
2021-09-30 20:03:29 +0300

    some changes in header + tests up

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
+import { IUser, Role } from '../models/user/user.model';
 
 @Component({
   selector: 'sis-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
 
-  constructor() { }
+  user$: Observable<IUser>;
+  isAuth$: Observable<boolean>;
+  isEmployer$: Observable<boolean>;
+
+  constructor(authService: AuthService) {
+    this.user$ = authService.user$;
+    this.isAuth$ = authService.isAuth$;
+    this.isEmployer$ = this.user$.pipe(map(e => e && e.Role === Role.Employer));
+  }
 
 }
